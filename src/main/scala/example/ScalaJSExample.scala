ee7887b981d2a024e603ac208138ca19f08117e9
joriscode <devil-gate@hotmail.fr>
2014-04-06 23:35:19 +0200

    Simplification of the implementation

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -5,7 +5,6 @@ import scala.collection.mutable
 import scala.scalajs.js.Any._
 import scala.scalajs.js.Math
 import annotation.JSExport
-// import org.scalajs.dom.MouseEvent
 
 object Color{
   def rgb(r: Int, g: Int, b: Int) = s"rgb($r, $g, $b)"
@@ -44,137 +43,126 @@ case class Point(x: Double, y: Double){
   }
 }
 
-abstract class Action
-case class ActionMouseClick(pos: Point) extends Action{ /* override def toString = "" */ }
-case class ActionMouseDoubleClick() extends Action{ }
-case class ActionMouseDrag(pos1: Point, pos2: Point) extends Action{ }
-
-class TreeHolder(canvasName: String, gameMaker: (Point, () => Unit) => Game){
-  private[this] val canvas = dom.document.getElementById(canvasName).asInstanceOf[dom.HTMLCanvasElement]
-  private[this] val bounds = Point(canvas.width, canvas.height)
-  private[this] val keys = mutable.Set.empty[Action]
-  var game: Game = gameMaker(bounds, () => resetGame())
-
-  // canvas.onkeydown = {(e: dom.KeyboardEvent) =>
-  //   keys.add(e.keyCode.toInt)
-  //   if (Seq(32, 37, 38, 39, 40).contains(e.keyCode.toInt)) e.preventDefault()
-  //   message = None
-  // }
-  // canvas.onkeyup = {(e: dom.KeyboardEvent) =>
-  //   keys.remove(e.keyCode.toInt)
-  //   if (Seq(32, 37, 38, 39, 40).contains(e.keyCode.toInt)) e.preventDefault()
-  // }
-
-  // canvas.onkeyup = {(e: dom.KeyboardEvent) =>
-    
-  //   var ctx = canvas.getContext("2d");
-  //   ctx.fillStyle = "#FF0000";
-  //   ctx.fillRect(0,0,150,75);
-  //   e.preventDefault()
-  // }
-
-  // canvas.onclick = {
-  canvas.onmousedown = {
-
-    (e: dom.MouseEvent) =>
-      keys.add(ActionMouseClick(Point(100, 100))) // location(e)
-      e.preventDefault()
-  }
-  
-  canvas.addEventListener("mousedown", (e:dom.Event) => e match { //wtf can't do partial function 
-		case e:dom.MouseEvent => 
-			dom.alert(e.clientX + "," + e.clientY)
-			keys.add(ActionMouseClick(Point(e.clientX, e.clientY)))
-	})
-
-  //  canvas.onMouseDown = {
-  //   (e: dom.MouseEvent) =>
-  //     val pos1 = location(e)
-      
-  //     canvas.onMouseDown = {
-  //       (e2: dom.MouseEvent) =>
-  //         val pos2 = location(e2)
-  //     }
-
-  //     keys.add(ActionMouseDrag(pos1, pos2)
-  //     e.preventDefault()
-  //     e2.preventDefault()
-  // }
-
-  def location(event: dom.MouseEvent): Point = {
-    Point(event.clientX - canvas.offsetLeft + dom.document.body.scrollLeft, event.clientY - canvas.offsetTop + dom.document.body.scrollTop)
-  }
-
-  canvas.onfocus = {(e: dom.FocusEvent) =>
-    active = true
-  }
-  canvas.onblur = {(e: dom.FocusEvent) =>
-    active = false
-  }
-
-  private[this] val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-  var active = false
-  var firstFrame = false
-  def update() = {
-    if (!firstFrame){
-      game.draw(ctx)
-      firstFrame = true
-    }
-    if (active && message.isEmpty) {
-      game.draw(ctx)
-      game.update(keys.toSet)
-    }else if (message.isDefined){
-      ctx.fillStyle = Color.Black
-      ctx.fillRect(0, 0, bounds.x, bounds.y)
-      ctx.fillStyle = Color.White
-      ctx.font = "20pt Arial"
-      ctx.textAlign = "center"
-      ctx.fillText(message.get, bounds.x/2, bounds.y/2)
-      ctx.font = "14pt Arial"
-      ctx.fillText("Press any key to continue", bounds.x/2, bounds.y/2 + 30)
-    }
-  }
-
-  var message: Option[String] = None
-  def resetGame(): Unit = {
-    message = game.result
-    println("MESSAGE " + message)
-    game = gameMaker(bounds, () => resetGame())
-  }
-  ctx.font = "12pt Arial"
-  ctx.textAlign = "center"
-}
-
-abstract class Game{
-  var result: Option[String] = None
-  def update(keys: Set[Action]): Unit
-
-  def draw(ctx: dom.CanvasRenderingContext2D): Unit
-
-  implicit class pimpedContext(val ctx: dom.CanvasRenderingContext2D){
-    def fillCircle(x: Double, y: Double, r: Double) = {
-      ctx.beginPath()
-      ctx.arc(x, y, r, 0, math.Pi * 2)
-      ctx.fill()
-    }
-    def strokePath(points: Point*) = {
-
-      ctx.beginPath()
-      ctx.moveTo(points.last.x, points.last.y)
-      for(p <- points){
-        ctx.lineTo(p.x, p.y)
-      }
-      ctx.stroke()
-    }
-  }
-}
 
 @JSExport
 object ScalaJSExample {
   @JSExport
   def main(): Unit = {
-    val treeTex = new TreeHolder("treePanel", TreeTex)
-    val games = Seq(treeTex)
-    dom.setInterval(() => games.foreach(_.update()), 15)
+    // val treeTex = new TreeHolder("treePanel", TreeTex)
+    val treeTex = new TreeTex("treePanel")
+    // val games = Seq(treeTex)
+    // dom.setInterval(() => games.foreach(_.update()), 15)
+    dom.setInterval( () => (treeTex.update()), 15 )
   }
+}
+
+
+
+case class Node(var pos: Point, var radius: Int, var color: String, var text: String, var parents: List[Node], var children: List[Node]) // may use (named & ) default args
+
+// case class TreeTex(canvasName: String, bounds: Point, resetGame: () => Unit) {
+case class TreeTex(canvasName: String) {
+  private[this] val canvas = dom.document.getElementById(canvasName).asInstanceOf[dom.HTMLCanvasElement]
+  private[this] val bounds = Point(canvas.width, canvas.height)
+  // private[this] val keys = mutable.Set.empty[Action]
+
+  var nodeList: List[Node] = List(Node(Point(50, 50), 20, Color.Blue, "Root", Nil, Nil))
+  var curNode = nodeList.head
+
+  canvas.addEventListener("mousedown", (e:dom.Event) => e match { //wtf can't do partial function 
+    case e:dom.MouseEvent => 
+      dom.alert(e.clientX + "," + e.clientY)
+      // keys.add(ActionMouseClick(Point(e.clientX, e.clientY)))
+  })
+
+  private[this] val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
+  draw()
+
+
+
+
+
+  def draw() = {
+    ctx.fillStyle = Color.Black
+    ctx.fillRect(0, 0, bounds.x, bounds.y)
+
+    ctx.fillStyle = Color.White
+    ctx.strokeStyle = Color.White
+
+    nodeList.foreach{ n => drawNode(n) }
+    // draw branches
+  }
+
+  def drawNode(node: Node) = {
+    ctx.beginPath()
+    ctx.arc(node.pos.x, node.pos.y, node.radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = node.color
+    ctx.fill()
+    ctx.lineWidth = 5
+    ctx.strokeStyle = Color.White // Warning: hard coded
+    ctx.stroke()
+
+    ctx.fillText(node.text, node.pos.x, node.pos.y) // may need to be centered on the node
+    // ...
+
+  }
+
+  def getNodeAt(click: Point): List[Node] = {
+    // for (n <- nodeList; if isNode(click, n)) yield n
+    nodeList.filter(n => isNode(n, click))
+  }
+
+  def isNode(node: Node, click: Point): Boolean = {
+    val x = click.x - node.pos.x
+    val y = click.x - node.pos.y
+    (x*x) + (y*y) <= node.radius
+  }
+
+
+  def update(): Unit = {
+    // dom.alert("update")
+    // draw
+    
+  }
+
+
+  // def update(keys: Set[Action]): Unit = {
+  //   dom.alert("click")
+  //   val node = keys.head
+    
+
+  //   node match {
+  //     case a: ActionMouseClick => simpleClick(a.pos)
+  //     // case ActionMouseDoubleClick => 
+  //     case a: ActionMouseDrag => drag(a.pos1, a.pos2)
+  //     // case _ => // throw error ?? in JS ??
+  //   }
+    
+  // }
+
+  def simpleClick(pos: Point) = {
+    val nodes = getNodeAt(pos)
+
+    if(!nodes.isEmpty && curNode == null){ // select the node
+      curNode = nodes.head
+
+    } else if(!nodes.isEmpty && curNode != null) { // link curNode and nodes.head
+      curNode.children ::: List(nodes.head)
+
+    } else { // create new node
+      nodeList ::: List(Node(pos, 20, Color.Blue, "node", Nil, Nil))
+      
+    }
+  }
+
+  def drag(pos1: Point, pos2: Point){
+    val nodes = getNodeAt(pos1)
+
+    if(!nodes.isEmpty){
+      curNode = nodes.head
+      nodes.head.pos = pos2
+    }
+  }
+
 }
