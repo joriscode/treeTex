7dff979d670b127157af534970a674bde6dc8a0f
begeric <begueteric@gmail.com>
2014-04-15 15:27:28 +0200

    Add scalatest to sbt

--- project/Build.scala
+++ project/Build.scala
@@ -4,10 +4,16 @@ import scala.scalajs.sbtplugin.ScalaJSPlugin._
 import ScalaJSKeys._
 
 object Build extends sbt.Build {
+
+	resolvers ++= Seq(
+	  "Sonatype OSS Releases"  at "http://oss.sonatype.org/content/repositories/releases/"
+	)
+
   lazy val root = project.in(file(".")).settings(
     scalaJSSettings: _*
   ).settings(
     name := "games",
-    libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.3"
+    libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.3",
+	libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
   )
 }
